5ce4cc0be6253f011e0512c16cc4276aba4c541a
gandhimansi91 <128670040+gandhimansi91@users.noreply.github.com>
2023-10-28 01:44:06 +0530

    Add files via upload

---
+++ EngineerDestAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using DatabaseProject.Interfaces;
+using DatabaseProject.Models;
+using DatabaseProject.DatabaseContext;
+using DatabaseProject.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EngineerDestAPI.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _EmployeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _EmployeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+
+        public ActionResult GetEmployees()
+        {
+            try
+            {
+                var employees = _EmployeeRepository.GetEmployees();
+                return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+
+        [HttpGet]
+
+        public ActionResult GetEmployeeById(int Id)
+        {
+            try
+            {
+                var employees = _EmployeeRepository.GetEmployeeById(Id);
+                return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public ActionResult AddEmployee(Employee employee)
+        {
+            try
+            {
+                var addedEmployee = _EmployeeRepository.AddEmployee(employee);
+                return Ok(addedEmployee);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex.Message);
+            }
+        }
+    }
+}
