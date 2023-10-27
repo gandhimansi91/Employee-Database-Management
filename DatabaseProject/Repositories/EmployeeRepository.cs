6d2b7d6254fe840a4f2364f0bac850178f9cbe98
gandhimansi91 <128670040+gandhimansi91@users.noreply.github.com>
2023-10-28 01:47:58 +0530

    Added Database folder

---
+++ DatabaseProject/Repositories/EmployeeRepository.cs
@@ -0,0 +1,40 @@
+﻿using DatabaseProject.DatabaseContext;
+using DatabaseProject.Interfaces;
+using DatabaseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseProject.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly SqlServerContext _SqlServerContext;
+
+        public EmployeeRepository(SqlServerContext sqlServerContext)
+        {
+            _SqlServerContext = sqlServerContext;
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            var lstEmployees = _SqlServerContext.Employees.ToList();
+            return lstEmployees;
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            var employee = _SqlServerContext.Employees.FirstOrDefault(x => x.EmployeeId == id);
+            return employee;
+        }
+
+        public Employee AddEmployee(Employee employee) 
+        {
+            _SqlServerContext.Employees.Add(employee);
+            _SqlServerContext.SaveChanges();
+            return employee;
+        }
+    }
+
+}
